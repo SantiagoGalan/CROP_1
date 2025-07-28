e8efbbf2a10ad29ed132bed8f65d067016381f8f
SantiagoGalan <sgalan@fi.uba.ar>
2025-07-28 00:01:38 -0300

    Se entrenaron y guardaron modelos.
    Se empezaron los experimientros sobre el CVAE aisaldo. Proximamente sobre el modelo completo. Empezar a escribir sobre alguno de esos experimentos

--- custom_layers/ReshapeLayer.py
+++ custom_layers/ReshapeLayer.py
@@ -1,9 +1,7 @@
 
 from keras.layers import Layer
 import tensorflow as tf
-from keras.saving import register_keras_serializable
 
-@register_keras_serializable()
 class ReshapeLayer(Layer):
     def __init__(self, target_shape, **kwargs):
         super(ReshapeLayer, self).__init__(**kwargs)
@@ -11,11 +9,3 @@ class ReshapeLayer(Layer):
 
     def call(self, inputs):
         return tf.reshape(inputs, [-1, *self.target_shape])
-
-    def get_config(self):
-        config = super().get_config().copy()
-        config.update({
-            'target_shape': self.target_shape,
-        })
-        return config
-
