41d2a830737d6e15e9f32d7d30a13a529f2d738e
galsan <sgalan@fi.uba.ar>
2025-10-15 16:31:38 -0300

    Normalization is now in loop

--- CROP_models/crop.py
+++ CROP_models/crop.py
@@ -233,20 +233,26 @@ class crop:
             )
             self.alpha_2 *= self.beta
 
+            eps = 1e-6
+            mask_sum = tf.maximum(mask_source1 + mask_source2, eps)
+            m1 = mask_source1 / mask_sum
+            m2 = mask_source2 / mask_sum
+
+            # Aplicar máscaras normalizadas para la próxima iteración
+            reconstructed_source1 = tf.clip_by_value(2.0 * mixed_input * m1, 0.0, 1.0)
+
             # Estimación de la fuente 2
             reconstructed_source2, mask_source2, predictions_2 = self.best_filtered_var_sigmoid(
                 reconstructed_source1, mixed_input, self.alpha_1
             )
             self.alpha_1 *= self.beta
 
-        eps = 1e-6
-        mask_sum = tf.maximum(mask_source1 + mask_source2, eps)
-        m1 = mask_source1 / mask_sum
-        m2 = mask_source2 / mask_sum
+            eps = 1e-6
+            mask_sum = tf.maximum(mask_source1 + mask_source2, eps)
+            m1 = mask_source1 / mask_sum
+            m2 = mask_source2 / mask_sum
 
-        # Aplicar máscaras normalizadas para la próxima iteración
-        reconstructed_source1 = tf.clip_by_value(2.0 * mixed_input * m1, 0.0, 1.0)
-        reconstructed_source2 = tf.clip_by_value(2.0 * mixed_input * m2, 0.0, 1.0)
+            reconstructed_source2 = tf.clip_by_value(2.0 * mixed_input * m2, 0.0, 1.0)
 
         (
             best_prediction_source1,
